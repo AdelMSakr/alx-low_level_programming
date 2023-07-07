50b275c42005f5ca2498491b84905f540bd07ef8
AdelMSakr <adel.mohamedsakr@gmail.com>
2023-07-07 09:37:59 -0700

    f3

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+
 /**
  * main - Write a program that prints all arguments it receives
  *        All arguments should be printed, including the first on
  *
- * @argc: This is the argument count 
+ * @argc: This is the argument count
  * @argv: This is the argument vector
  *
  * Return: 0;
- */
+*/
+
 int main(int argc, char *argv[])
 {
 	int count;
